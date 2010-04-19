3ed14678eb51e6984dd80e2aef0263f90801d260
Choan Galvez <choan.galvez@gmail.com>
2010-04-19 15:40:13 +0200

    Added ukulele augmented seventh chords

---
+++ lib/fretboards/ukulele/chords/augmented_seventh.rb
@@ -0,0 +1,42 @@
+module Fretboards::Ukulele::Chords::AugmentedSeventh
+  
+  
+  
+  Fretboards.add(self.name, "root_fourth") do 
+    ::Fretboards::Fretboard.new(:strings => 4, :fret_start => 1, :fret_end => 5, :position => :relative) do
+      mark(:string => 4) { mark :fret => 2, :symbol => :root }
+      mark(:string => 3) { mark :fret => 5 }
+      mark(:string => 2) { mark :fret => 3 }
+      mark(:string => 1) { mark :fret => 4 }
+    end
+  end
+
+
+  Fretboards.add(self.name, "root_second") do
+    ::Fretboards::Fretboard.new(:strings => 4, :fret_start => 1, :fret_end => 5, :position => :relative) do
+      mark :string => 4, :fret => 3
+      mark :string => 3, :fret => 4
+      mark :string => 2, :fret => 2, :symbol => :root
+      mark :string => 1, :fret => 5
+    end
+  end
+
+  Fretboards.add(self.name, "root_third") do
+    ::Fretboards::Fretboard.new(:strings => 4, :fret_start => 1, :fret_end => 5, :position => :relative) do
+      mark :string => 4, :fret => 4
+      mark :string => 3, :fret => 3, :symbol => :root
+      mark :string => 2, :fret => 3
+      mark :string => 1, :fret => 4
+    end
+  end
+
+  Fretboards.add(self.name, "root_first") do
+    ::Fretboards::Fretboard.new(:strings => 4, :fret_start => 1, :fret_end => 5, :position => :relative) do
+      mark :string => 4, :fret => 2
+      mark :string => 3, :fret => 3
+      mark :string => 2, :fret => 3
+      mark :string => 1, :fret => 2, :symbol => :root
+    end
+  end
+
+end
