e6502cf9f473c54428b6b2a07137a2b0ba94427d
Choan Galvez <choan.galvez@gmail.com>
2010-09-27 20:17:35 +0200

    Fix arppegio

--- lib/fretboards/ukulele/arpeggios/major_seventh.rb
+++ lib/fretboards/ukulele/arpeggios/major_seventh.rb
@@ -2,12 +2,13 @@ module Fretboards::Ukulele::Arpeggios::MajorSeventh
     
   Fretboards.add(self.name, "first_position") do
     ::Fretboards::Fretboard.new(:strings => 4, :fret_start => 1, :fret_end => 5, :position => :relative) do
-        mark :string => 4, :fret => 1, :symbol => :root
-        mark :string => 4, :fret => 5
-        mark :string => 3, :fret => 3
-        mark :string => 2, :fret => 3
-        mark :string => 2, :fret => 4, :symbol => :root
-        mark :string => 1, :fret => 3
+        mark :string => 4, :fret => 1
+        mark :string => 4, :fret => 2, :symbol => :root
+        mark :string => 3, :fret => 1
+        mark :string => 3, :fret => 4
+        mark :string => 2, :fret => 4
+        mark :string => 2, :fret => 5, :symbol => :root
+        mark :string => 1, :fret => 4
       end
     end
     
