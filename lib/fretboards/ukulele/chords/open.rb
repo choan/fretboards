a6d6fbe28f863252bb439ce9cf0e7b33c2d97508
Choan Galvez <choan.galvez@gmail.com>
2011-01-23 22:25:02 +0100

    F chord

--- lib/fretboards/ukulele/chords/open.rb
+++ lib/fretboards/ukulele/chords/open.rb
@@ -18,4 +18,13 @@ module Fretboards::Ukulele::Chords::Open
     end
   end
 
+  Fretboards.add(self.name, "F") do 
+    ::Fretboards::Fretboard.new(:strings => 4, :fret_start => 0, :fret_end => 4, :position => :absolute) do
+      mark :string => 4, :fret => 2, :finger => 2, :function => :third
+      mark :string => 3, :fret => 0, :function => :fifth
+      mark :string => 2, :fret => 1, :finger => 1, :function => :root
+      mark :string => 1, :fret => 0, :function => :third
+    end
+  end
+
 end
